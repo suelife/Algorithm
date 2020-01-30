07794d8e34f3ca7fe917a9ae6fa49fc96e77bb03
windkuo <suelife@gmail.com>
2020-01-30 13:42:25 +0800

    Done

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,6 +1,23 @@
-from random import shuffle
+from random import shuffle, randint
 
-num = 10
-data = [n for n in range(num+1)]
-shuffle(data)
-print('origin:', data)
+
+def bubble_sort(data: list):
+    count = 1
+    while True:
+        flag = False
+        for i in range(len(data)-1):
+            if data[i] > data[i+1]:
+                flag = True
+                data[i], data[i+1] = data[i+1], data[i]
+        if flag == False:
+            break
+        print('第', count, '回', data)
+        count += 1
+
+
+if __name__ == "__main__":
+    num = 10
+    data = [n for n in range(1, num+randint(1, 2))]
+    shuffle(data)
+    print('origin:', data)
+    bubble_sort(data)
