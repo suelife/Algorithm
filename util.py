e4a120a0b96c08fbe3e6410233bbe87f346b3f46
windkuo <suelife@gmail.com>
2020-02-03 11:48:31 +0800

    Add comment

--- util.py
+++ util.py
@@ -1,6 +1,11 @@
 from random import shuffle, randint
 
 def test_func(func):
+    '''Create data to put in the function.
+
+    Arguments:
+        func: A function for data input.
+    '''
     num = 10
     data = [n for n in range(1, num+randint(1, 2))]
     shuffle(data)
